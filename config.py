8322591b9cd7d030dd5bc2d99b0c3e519d06eb15
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-12-28 11:55:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/9db6ae360e0a7b833abff.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3d0d7d23d3a7fb86b442e.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/edf8f2bdc919ae8584650.jpg)
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/6b26a7973dbd91a1a0fe2.jpg")
 
 SESSION = getenv("SESSION", None)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Friends_chatting_Group_Sigma")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDRD_BOT")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "BRANDED KIGN")  
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴛʜᴜɴᴅᴇʀ 〆 ᴍᴜꜱɪᴄ")  
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
