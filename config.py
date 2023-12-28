7b6a247fb7cbd53579b2c50fae101d012bc42217
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-12-28 11:55:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/edf8f2bdc919ae8584650.jpg)
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/edf8f2bdc919ae8584650.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/6b26a7973dbd91a1a0fe2.jpg")
 
 SESSION = getenv("SESSION", None)
